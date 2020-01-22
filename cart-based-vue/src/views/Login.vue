178f905e58b3537b6ead012beae3fe9cf16f278c
BlackCodingCat <sjq15251852316@163.com>
2020-01-22 18:11:06 +0800

    使用cube-ui搭建登录页面

--- cart-based-vue/src/views/Login.vue
+++ cart-based-vue/src/views/Login.vue
@@ -1,15 +1,87 @@
 <template>
-	<div>
-		Login
-	</div>
+  <div class="login-container">
+    <img class="banner" src="../assets/images/panda.jpg" alt="" />
+    <div class="login-box">
+      <cube-form
+        :model="model"
+        :schema="schema"
+        @submit.prevent="handleLogin"
+        @validate="handleValidate"
+      >
+      </cube-form>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		
-	}
+export default {
+  data() {
+    return {
+      model: {
+        username: "",
+        pwd: ""
+      },
+
+      schema: {
+        fields: [
+          {
+            type: "input",
+            modelKey: "username",
+            label: "用户名",
+            props: {
+              placeholder: "请输入用户名"
+            },
+            rules: {
+              required: true
+            },
+            trigger: "blur"
+          },
+          {
+            type: "input",
+            modelKey: "pwd",
+            label: "密码",
+            props: {
+              placeholder: "请输入密码",
+              eye: {
+                open: true
+              }
+            },
+            rules: {
+              required: true
+            },
+            trigger: "blur",
+          },
+          {
+            type: "submit",
+            label: "登录"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+	  handleLogin(e){
+		  // 阻止表单默认提交行为
+		  e.preventDefault();
+		  // 请求登录
+		  
+	  },
+
+	  handleValidate(ret){
+		  console.log(ret);	  
+	  }
+  },
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.login-container {
+  box-sizing: border-box;
+  padding: 0 20px;
 
+  .banner {
+    width: 100%;
+    border-radius: 8px;
+  }
+}
 </style>
