d8cacca440aada667987c00326ac48730788be80
BlackCodingCat <sjq15251852316@163.com>
2020-01-22 17:26:46 +0800

    安装cube-ui

--- cart-based-vue/vue.config.js
+++ cart-based-vue/vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-	publicPath: './'
+    publicPath: './',
+
+    css: {
+      loaderOptions: {
+        stylus: {
+          'resolve url': true,
+          'import': []
+        }
+      }
+    },
+
+    pluginOptions: {
+      'cube-ui': {
+        postCompile: true,
+        theme: false
+      }
+    }
 }
